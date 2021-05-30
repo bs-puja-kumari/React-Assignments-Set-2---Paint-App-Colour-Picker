ad0b3add05ca6ae405ab9a73f4ef4cf206a349df
bs-puja-kumari <83964038+bs-puja-kumari@users.noreply.github.com>
2021-05-30 16:22:03 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,11 @@
-
+import React, {useState} from 'react'
+function Selection(props){
+  const {applyColor}=props;
+  const[selectionStyle,updateSlectionStyle]=useState({backgroung:''});
+  return(
+    <div className="fix-box" style={selectionStyle} onClick={()=>{applayColor(updateSelectionStyle)}}>
+    <h2 class="subheading">Selection</h2>
+    </div>
+  );
+}
+export default Selection;
